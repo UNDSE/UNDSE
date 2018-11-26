cbee30b8fd671f7603850f3bf5bf74f3c77d8a2d
UNDSE <45343220+UNDSE@users.noreply.github.com>
2018-11-26 09:42:05 +0530

    committed.
    ¶ Copyright © 2014 - 2018, Sequømics Research, All rights reserved.
    ｢०｣ • ॐ — by Prabhat Kumar.

---
+++ __init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ————————————————————————————————————————————————————————————————————————————
+# Copyright © 2014 - 2018, Sequømics Research, All rights reserved.
+# Copyright © 2014 - 2018, Sequømics Corporation. All rights reserved.
+# ————————————————————————————————————————————————————————————————————————————
+# Licensed under the GNU GENERAL PUBLIC LICENSE (the "License, version 3.0");
+# you may not use this file except in compliance with the License.
+# ————————————————————————————————————————————————————————————————————————————
